58d25b06e2bdd54957b7fab9c4587bacadecdd88
Nick Goudeau <ngoudeau2012@gmail.com>
2021-02-22 22:10:14 -0800

    background and headshot put in

---
+++ src/components/circleImg/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function CircleImg(props) {
+    return(
+        <dvi>
+            <img className="circle" src={props.img}></img>
+        </dvi>
+    )
+}
+
+export default CircleImg
